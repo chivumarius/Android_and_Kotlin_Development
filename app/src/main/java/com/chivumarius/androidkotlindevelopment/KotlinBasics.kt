3a403946a090027e8861e8aa82dd415ad67860b7
Chivu Marius <chivumarius@yahoo.com>
2023-03-29 13:02:33 +0300

    Assignment Operators

--- app/src/main/java/com/chivumarius/androidkotlindevelopment/KotlinBasics.kt
+++ app/src/main/java/com/chivumarius/androidkotlindevelopment/KotlinBasics.kt
@@ -194,11 +194,14 @@ fun main (){
     // ♦ "String Template" Expression ("String Interpolation")    
     println("► Is 5 Less than 3?  ${5 < 3}")
 
+
     // --------------------------------------------
     // (6.4) "GREATER THAN" OPERATOR (">"):
     // ♦ "String Template" Expression ("String Interpolation")   
     println("► Is 5 Greater than 3?  ${5 > 3}")
-
+   
+   
+    // --------------------------------------------
     // (6.5) "LESS OR EQUAL" OPERATOR ("<="):
     // ♦ "String Template" Expression ("String Interpolation")   
     println("► Is 8 Less or Equal 8?  ${8 <= 8}")
@@ -208,4 +211,55 @@ fun main (){
     // ♦ "String Template" Expression ("String Interpolation")   
     println("► Is 6 Greater or Equal 5?  ${6 >= 5}")
     // ===============================================
+
+
+    
+    // ===============================================
+    // (VII) ASSIGNMENT   OPERATORS (+=, -=, *=, /=, %=)
+
+    // --------------------------------------------
+    // (7.1) "ADDITION ASSIGNMENT" OPERATOR ("+="):
+    //          → "Add" a "Value" to the "Same Variable"    
+    var addNumber = 4
+    // var addNumber = addNumber + 8
+    addNumber += 8
+    println("► addNumber is $addNumber")
+
+
+    // --------------------------------------------
+    // (7.2) "SUBSTRACTION ASSIGNMENT" OPERATOR ("-="):
+    //          → "Subtract" a "Value" to the "Same Variable"    
+    var subtractNumber = 9
+    // var subtractNumber = subtractNumber - 7
+    subtractNumber -= 7
+    println("► subtractNumber is $subtractNumber")
+
+
+    // --------------------------------------------
+    // (7.3) "MULTIPLICATION ASSIGNMENT" OPERATOR ("*="):
+    //          → "Multiply" a "Value" to the "Same Variable"    
+    var multiplyNumber = 3
+    // var multiplyNumber = multiplyNumber * 2
+    multiplyNumber *= 2
+    println("► multiplyNumber is $multiplyNumber")
+
+
+    // --------------------------------------------
+    // (7.4) "DIVISION ASSIGNMENT" OPERATOR ("/="):
+    //          → "Dividing" a "Value" to the "Same Variable"    
+    var dividingNumber = 9
+    // var dividingNumber = dividingNumber / 3
+    dividingNumber /= 3
+    println("► dividingNumber is $dividingNumber")
+
+
+    // --------------------------------------------
+    // (7.5) "MODULUS ASSIGNMENT" OPERATOR ("%="):
+    //          → "Remainder" of "Dividing" a "Value" to the "Same Variable"    
+    var remainderOfDividingNumber = 10
+    // var remainderOfDividingNumber = remainderOfDividingNumber % 3
+    remainderOfDividingNumber %= 3
+    println("► remainderOfDividingNumber is $remainderOfDividingNumber")
+    // ===============================================
+
 }
