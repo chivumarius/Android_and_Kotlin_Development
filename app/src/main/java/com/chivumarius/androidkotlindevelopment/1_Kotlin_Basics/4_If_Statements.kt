58bae9926998c1753df9263698b34978b7c91a12
Chivu Marius <chivumarius@yahoo.com>
2023-03-30 10:50:21 +0300

    If Statements

---
+++ app/src/main/java/com/chivumarius/androidkotlindevelopment/1_Kotlin_Basics/4_If_Statements.kt
@@ -0,0 +1,39 @@
+/*
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+            ____________________________        
+                
+                ○  KOTLIN  BASICS  ○ 
+                ____________________
+
+                - "IF" STATEMENTS -
+            ____________________________
+            
+            
+
+
+    ♦ Note ♦
+        We Run the File
+        → by pressing the "Play" Button
+        → on the "Left" of the "File".
+
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+
+package com.chivumarius.androidkotlindevelopment
+
+// ♦ Kotlin Application Start Function:
+fun main (){
+    // ♦ Variables:
+    var heightPerson1 = 170
+    var heightPerson2 = 190
+
+    // ♦ Condition:
+    if( heightPerson1 > heightPerson2){
+        println("Use raw force")
+    } else if(heightPerson1 == heightPerson2){
+        println("Use your power technique 1400")
+    } else {
+        println("Use technique")
+    }
+
+}
