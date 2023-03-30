7d21d38e14efcd2342b392726c1d0a3a2f1da572
Chivu Marius <chivumarius@yahoo.com>
2023-03-30 17:51:55 +0300

    IF Statement

--- app/src/main/java/com/chivumarius/androidkotlindevelopment/1_Kotlin_Basics/4_If_Statements.kt
+++ app/src/main/java/com/chivumarius/androidkotlindevelopment/1_Kotlin_Basics/4_If_Statements.kt
@@ -23,17 +23,68 @@ package com.chivumarius.androidkotlindevelopment
 
 // ♦ Kotlin Application Start Function:
 fun main (){
+    
+    // ===============================================
+    // (1) EX - "INTEGER" VALUES:
+    // -----------------------------------------------
     // ♦ Variables:
-    var heightPerson1 = 170
+    var heightPerson1 = 190
     var heightPerson2 = 190
 
-    // ♦ Condition:
+    // ♦ Condition Checked:
     if( heightPerson1 > heightPerson2){
-        println("Use raw force")
+        println("► Use raw force")
     } else if(heightPerson1 == heightPerson2){
-        println("Use your power technique 1400")
+        println("► Use your power technique 1400")
     } else {
-        println("Use technique")
+        println("► Use technique")
     }
+    // -----------------------------------------------
 
+
+
+    // -----------------------------------------------
+    // ♦ Variable:
+    var age = 17
+
+
+    // ♦ Condition Checked:
+    if(age >= 21) {
+       println("► Now you may drink in the EU!") 
+    } else  if(age >= 18) {
+        println("► You may vote now!") 
+    } else  if(age >= 16) {
+        println("► You may drive now!") 
+    } else {
+        println("► You are too young!") 
+    }
+    // -----------------------------------------------
+    // ===============================================
+
+
+
+     // ===============================================
+    // (2) EX - "STRINGS" VALUES:
+    var name = "Marius"
+
+    if(name == "Marius"){
+        println("► Welcom home Marius!")
+    } else {
+        println("► Who are you?")
+    }
+    // ===============================================
+
+
+
+    
+    
+
+    // ===============================================
+    // (3) EX - "BOOLEAN" VALUES:
+    // -----------------------------------------------
+    var isRainy = true
+
+    if(isRainy)
+        println("► It`s rainy!")
+    // ==
 }
