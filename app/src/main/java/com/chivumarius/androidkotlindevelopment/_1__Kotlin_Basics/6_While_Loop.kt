fb6c9d7901541032f57c81f703423cda272a90ad
Chivu Marius <chivumarius@yahoo.com>
2023-04-02 10:13:43 +0300

    While Loop

---
+++ app/src/main/java/com/chivumarius/androidkotlindevelopment/_1__Kotlin_Basics/6_While_Loop.kt
@@ -0,0 +1,52 @@
+/*
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+            ____________________________        
+                
+                ○  KOTLIN  BASICS  ○ 
+                ____________________
+
+                - "WHILE(){}" LOOP -
+            ____________________________
+            
+            
+
+
+    ♦ "Loops"
+        → can execute a "Block of Code" 
+        → as long as a "Specified Condition" 
+        → is "Reached".
+
+
+
+
+    ♦ The "While" Loop 
+        → "Loops" through a "Block of Code" 
+        → as long as a "Specified Condition" 
+        → is "True".
+
+
+
+    ♦ The "While(){}" Loop "Syntax":
+    _______________________________________
+        
+        while (condition) {
+          // Code Block to be Executed
+        }
+    _______________________________________
+
+
+    ♦ Note ♦
+        We Run the File
+        → by pressing the "Play" Button
+        → on the "Left" of the "File".
+
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+
+package com.chivumarius.androidkotlindevelopment._Sec_1__Kotlin_Basics
+
+
+// ♦ Kotlin Application Start Function:
+fun main (){
+
+}
