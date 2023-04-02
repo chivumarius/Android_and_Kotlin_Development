0146b94dacffad1625b476bb47f9b7df7e1ad8d5
Chivu Marius <chivumarius@yahoo.com>
2023-04-02 17:26:19 +0300

    Functions → Parameters,  Arguments and Return

---
+++ app/src/main/java/com/chivumarius/androidkotlindevelopment/_1__Kotlin_Basics/9_Functions.kt
@@ -0,0 +1,70 @@
+/*
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+            ____________________________        
+                
+                ○  KOTLIN  BASICS  ○ 
+                ____________________
+
+                  - "FUNCTIONS" -
+            ____________________________
+            
+            
+
+
+    ♦ "Function" 
+        → is a "Block of Code" 
+        → which "Only Runs" 
+        → when it is "Called".
+
+
+
+    ♦ "Functions" 
+        → are used to "Perform Certain Actions", 
+        → and they are also known as "Methods".
+
+
+
+    ♦ "Data" (Known as "Parameters")
+        → can be "Passed" 
+        → to a "Function".
+
+
+
+    ♦ Kotlin "functions" 
+        → are Declared 
+        → using the "fun" Keyword:
+
+
+    ♦ The "functions" "Syntax":
+    _______________________________________
+        
+       fun functionName(Parameters) {
+          // Code Block to be Executed
+        }
+    _______________________________________
+
+
+
+    ♦ Note ♦
+        We Run the File
+        → by pressing the "Play" Button
+        → on the "Left" of the "File".
+
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+
+package com.chivumarius.andro idkotlindevelopment._1__Kotlin_Basics
+
+
+
+// ♦ The "main(){}" Function 
+//      → as "Entry Point" of "Application":
+fun main (){
+    
+}
+
+
+// ♦ The "myFunction(){}" Function: 
+fun myFunction (){
+    
+}
