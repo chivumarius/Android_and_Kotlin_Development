158c1f6fb948cd2a6154dc0c53b1d9090d989241
Chivu Marius <chivumarius@yahoo.com>
2023-04-03 12:25:31 +0300

    Nullables: 'Elvis' Operator &  'Not Null Assertion" Operator & 'Safe Call' Operator

--- app/src/main/java/com/chivumarius/androidkotlindevelopment/_1__Kotlin_Basics/10_Nullables.kt
+++ app/src/main/java/com/chivumarius/androidkotlindevelopment/_1__Kotlin_Basics/10_Nullables.kt
@@ -60,7 +60,7 @@ package com.chivumarius.androidkotlindevelopment._1__Kotlin_Basics
 //      → as "Entry Point" of "Application":
 fun main (){
     // (1) Variable:
-    var name: String = "Marius"
+    // var name: String = "Marius"
 
     // ♦ We Cannot Assign "null" Value to Variable
     //      •► "Null can not be a value of a non-null type String"
@@ -75,7 +75,7 @@ fun main (){
 
 
     // ♦ Getting the "Length" of the "name" Variable:
-    var len = name.length
+    // var len = name.length
 
     // ♦ Option 1 (New) → to Access the "length" of the "null" Variable
     //      → by "?":
@@ -89,16 +89,42 @@ fun main (){
     //   → Executed "Only if there is" a Variable "Value"
     nullableName?.let { println(nullableName?.toLowerCase()) }
 
+
+
+
+
  
+    // (3) "ELVIS" OPERATOR ("?:")
+    //     → for Assigning "nullable" Values 
+    //     → to the "name" Variable
+    // ♦ If "nullableName" Value is "Empty" 
+    //   → then Use "Default Value" - "Guest"
+    var name2 = nullableName ?: "Guest"
+    // println("Name is $name2")
+
+    
+    
+    // (4) "NOT-NULL  ASSERTION" OPERATOR ("!!")
+    //     → which Converts "Any Type" 
+    //     → into a "Non-Null Type"
+    println(nullableName!!.toLowerCase())
+
+
+    // (5) "SAFE  CALL" OPERATOR (Used in "Classes"):
+    val wifesAge: String? = user?.wife?.age ?: 0
+
+
+
     // ♦ Option 2 (Old) → to Access the "length" of the "null" Variable
     /*
-    if(nullableName != null){
-        // ♦ Getting the "Length" of the "nullableName" Variable:
-        var len2 = nullableName.length
-    } else {
-        null
-    }
+        if(nullableName != null){
+            // ♦ Getting the "Length" of the "nullableName" Variable:
+            var len2 = nullableName.length
+        } else {
+            null
+        }
     */
+    
 }
  
  
