2aecc21a0b406d831cf2545934d7391e4a672959
Chivu Marius <chivumarius@yahoo.com>
2023-04-03 17:28:36 +0300

    Kotlin  OOP → Classes & Initializers

---
+++ app/src/main/java/com/chivumarius/androidkotlindevelopment/_2__Kotlin_OOP/1_Classes_and_Initializers.kt
@@ -0,0 +1,42 @@
+/*
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+            ______________________________        
+                
+                ○  KOTLIN  OOP  ○ 
+                ____________________
+
+            - "CLASSES & INITIALIZERS" -
+            ______________________________
+          
+
+            
+    ♦ A "class" 
+        → is a "Blueprint" 
+        → of an "Object".        
+    
+
+
+    ♦ Note ♦
+        We Run the File
+        → by pressing the "Play" Button
+        → on the "Left" of the "File".
+
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+
+package com.chivumarius.androidkotlindevelopment._1__Kotlin_Basics
+
+
+
+// ♦ The "main(){}" Function 
+//      → as "Entry Point" of "Application":
+fun main (){
+    
+}
+
+
+// ♦ Function "With Parameters" ("Input"): 
+fun average(a: Double, b: Double): Double{
+    // ♦ "Output"
+    return (a + b)/2
+} 
