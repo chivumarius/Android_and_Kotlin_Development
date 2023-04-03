41633d58ea7659b595f215df3b690c3eff961c3b
Chivu Marius <chivumarius@yahoo.com>
2023-04-03 10:32:08 +0300

    Nullables In Kotlin

---
+++ app/src/main/java/com/chivumarius/androidkotlindevelopment/_1__Kotlin_Basics/10_Nullables.kt
@@ -0,0 +1,104 @@
+/*
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+            ____________________________        
+                
+                ○  KOTLIN  BASICS  ○ 
+                ____________________
+
+                  - "NULLABLES" -
+            ____________________________
+            
+            
+
+
+    ♦ The Kotlin "Nullables" Type System
+        → is Aimed to "Eliminating" the "Danger" of "Null References", 
+        → also known as T"he Billion Dollar Mistake".
+
+
+
+    ♦ The only possible causes of an NPE in Kotlin are:
+
+        [1] An Explicit Call 
+            → to throw "NullPointerException()".
+
+
+        [2] Usage of the "!!" Operator. 
+
+
+        [3] "Data Inconsistency" 
+            → with regard to "Initialization", 
+            → such as when:
+
+                ► An Uninitialized "this" Available 
+                    → in a "Constructor" 
+                    → is "Passed" 
+                    → and U"sed Somewhere" (a "leaking this").
+
+                ► A "Superclass Constructor" 
+                    → Calls an "Open Member" 
+                    → whose "Implementation" in the "Derived Class" 
+                    → Uses an "Uninitialized State".
+
+
+
+
+
+    ♦ Note ♦
+        We Run the File
+        → by pressing the "Play" Button
+        → on the "Left" of the "File".
+
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+
+package com.chivumarius.androidkotlindevelopment._1__Kotlin_Basics
+
+
+
+// ♦ The "main(){}" Function 
+//      → as "Entry Point" of "Application":
+fun main (){
+    // (1) Variable:
+    var name: String = "Marius"
+
+    // ♦ We Cannot Assign "null" Value to Variable
+    //      •► "Null can not be a value of a non-null type String"
+    // name = null
+
+
+    // (2) "Nullable" Variable (by "?"):
+    var nullableName: String? = "Marius" 
+
+    // ♦ The "?" Allows us to Assign "null" Values:
+    // nullableName = null
+
+
+    // ♦ Getting the "Length" of the "name" Variable:
+    var len = name.length
+
+    // ♦ Option 1 (New) → to Access the "length" of the "null" Variable
+    //      → by "?":
+    var len2 = nullableName?.length
+    
+    // ♦ The "Use" of "Methods" → on "nullable" Variables:
+    // println(nullableName?.toLowerCase())
+
+    
+    // ♦ Using the "Safe Call" Operator
+    //   → Executed "Only if there is" a Variable "Value"
+    nullableName?.let { println(nullableName?.toLowerCase()) }
+
+ 
+    // ♦ Option 2 (Old) → to Access the "length" of the "null" Variable
+    /*
+    if(nullableName != null){
+        // ♦ Getting the "Length" of the "nullableName" Variable:
+        var len2 = nullableName.length
+    } else {
+        null
+    }
+    */
+}
+ 
+ 
