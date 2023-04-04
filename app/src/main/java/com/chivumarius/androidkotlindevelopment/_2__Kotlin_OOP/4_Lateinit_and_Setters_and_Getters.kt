d09f46a7f2e5139a289a48b941c2e6dcb6e5860a
Chivu Marius <chivumarius@yahoo.com>
2023-04-04 18:46:15 +0300

    Lateinit Keyword, Setters & Getters Methods

---
+++ app/src/main/java/com/chivumarius/androidkotlindevelopment/_2__Kotlin_OOP/4_Lateinit_and_Setters_and_Getters.kt
@@ -0,0 +1,108 @@
+/*
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+            __________________________________        
+                
+                 ○  KOTLIN  OOP  ○ 
+                  ____________________
+
+            - "LATEINIT & SETTERS & GETTERS" - 
+            __________________________________
+
+
+            
+    ♦ The "lateinit" Keyword 
+        → stands for “Late Initialization”. 
+
+        → When "Used" with a "Class Property", 
+        → the "lateinit" Modifier 
+        → keeps the "Property" from being "Initialized" 
+        → at the Time of its "Class' Object Construction".
+
+
+      
+    ♦ The "private" keyword 
+        → specifies that "Something" is "Accessible" 
+        → only "Inside" the "Class"
+
+
+
+    ♦ Note ♦
+        We Run the File
+        → by pressing the "Play" Button
+        → on the "Left" of the "File".
+
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+
+package com.chivumarius.androidkotlindevelopment._2__Kotlin_OOP
+
+
+
+
+// ♦ The "main(){}" Function
+//      → as "Entry Point" of "Application":
+fun main (){
+    // ♦ "Object"
+    var myCar = Car()
+    
+    // ♦ Accessing "Class Properties"
+    println("Brand is: ${myCar.myBrand}")
+    
+    // ♦ "Reassignment" of "Class Property Value":
+    // myCar.maxSpeed = 200
+    // myCar.maxSpeed = -5
+    myCar.maxSpeed = 220
+    println("MaxSpeed is: ${myCar.maxSpeed}")
+    
+    // ♦ "Setter" is "Private":
+    // myCar.myModel = "M7"  // ♦ We gat an Error
+
+    // ♦ "Getter" is "Not Private":
+    println("Model is: ${myCar.myModel}")
+}
+
+
+
+// ♦ "Class": 
+class Car (){
+    // ♦ The "lateinit" Keyword 
+    //   → for "Later Initialization" of the "Property"
+    lateinit var owner: String
+
+
+    // ♦ The "Constant Property":
+    val myBrand: String = "BMW"
+
+        // ♦ "Custom Getter":
+        get(){
+            return field.toLowerCase()    
+        }
+
+
+    // ♦ The "Property":
+    var maxSpeed: Int = 250
+        // ♦ "Default Getter":
+        // get() = field
+
+        // ♦ "Custom Setter":
+        set(value) {
+            field = if(value > 0) value 
+                else throw IllegalArgumentException("MaxSpeed cannot be less then 0!")  
+        }        
+
+
+         // ♦ The "Property":
+    var myModel: String = "M5"
+        // ♦ "Private Setter":
+        private set(value) {
+            field = value
+        }  
+
+
+    // ♦ The "Class Initializer":
+    init{
+        // ♦ Reassignment of "Property Values" → "Inside" the "Class":
+        this.myModel = "M3"
+        this.owner = "Andrei"
+    }
+} 
